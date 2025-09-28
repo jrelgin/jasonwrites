f33b38ef09eee87bee8ff532d117521e092265f5
Jason Elgin <jason@signllantern.com>
2025-09-28 08:26:57 -0400

    remove astro and vercel build files and ignore

--- .astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_Z2eHNBx from "../../assets/uploads/a-new-piece-of-writing/cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fa-new-piece-of-writing.mdoc";
-export default new Map([["../../assets/uploads/a-new-piece-of-writing/cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fa-new-piece-of-writing.mdoc", __ASTRO_IMAGE_IMPORT_Z2eHNBx]]);
-		
